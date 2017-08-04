f99180836f47f8a67da10a1f8e3a528fa0521912
Jared Johnson <jared@nuts.com>
2017-08-04 10:34:24 -0400

    Test overrides.

---
+++ client/src/comp/button/index.tsx
@@ -0,0 +1,3 @@
+import Button from 'antd/lib/button';
+
+export default Button;
--- client/src/root/app/index.tsx
+++ client/src/root/app/index.tsx
@@ -1,26 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { DatePicker, message } from 'antd';
+
+import Button from 'comp/button';
+import 'antd/dist/antd.less';
 
 interface AppProps {}
 
-interface AppState { date: string }
+interface AppState {}
 
 class App extends React.Component<AppProps, AppState> {
   props: AppProps;
-  state: AppState = { date: '' };
+  state: AppState;
 
-  handleChange = (date: any): void => {
-    message.info('Selected Date: ' + date.toString());
-    this.setState({ date });
-  }
-
-  render (): JSX.Element {
+  render(): JSX.Element {
     return (
-      <div style={{ width: 400, margin: '100px auto' }}>
-        <DatePicker onChange={value => this.handleChange(value)} />
-        <div style={{ marginTop: 20 }}>Date: {this.state.date.toString()}</div>
-      </div>
+      <Button type="primary">
+        Click me!
+      </Button>
     );
   }
 }
