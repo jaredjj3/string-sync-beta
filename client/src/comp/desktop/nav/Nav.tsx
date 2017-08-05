b7d44a25f6ddf9f687a5d09088945284494b4529
Jared Johnson <jared@nuts.com>
2017-08-04 23:46:47 -0400

    Setup the navbar.

--- client/src/comp/desktop/nav/Nav.tsx
+++ client/src/comp/desktop/nav/Nav.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
+import Logo from './logo';
 import Menu from 'antd/lib/menu';
-const { Item } = Menu;
 import Icon from 'comp/icon';
+import Row from 'comp/row';
+import Col from 'comp/col';
+
+import './_nav.scss';
+
+const { Item, ItemGroup } = Menu;
 
 interface NavProps {}
 
@@ -12,6 +18,7 @@ interface NavState {
 }
 
 enum NavKeys {
+  LOGO   = 'LOGO',
   SEARCH = 'SEARCH',
   HOME   = 'HOME',
   LOGIN  = 'LOGIN'
@@ -21,28 +28,36 @@ class Nav extends React.Component<NavProps, NavState> {
   state: NavState = { current: NavKeys.HOME };
 
   handleClick = (e: any): void => {
-    console.log('click ', e);
     this.setState({ current: e.key });
   }
 
   render(): JSX.Element {
     return (
-      <Menu
-        onClick={this.handleClick}
-        selectedKeys={[this.state.current]}
-        mode="horizontal"
-        style={{ lineHeight: '64px', fontSize: '24px' }}
-      >
-        <Item key={NavKeys.SEARCH}>
-          <Icon type="search" />
-        </Item>
-        <Item key={NavKeys.HOME}>
-          <Icon type="home" />
-        </Item>
-        <Item key={NavKeys.LOGIN}>
-          <Icon type="user" />
-        </Item>
-      </Menu>
+      <nav className="Nav--desktop">
+        <Row type="flex" justify="space-between" align="middle">
+          <Col xs={0} sm={0} md={8} lg={8} push={4}>
+            <Logo />
+          </Col>
+          <Col xs={0} sm={0} md={8} lg={8}>
+            <Menu
+              onClick={this.handleClick}
+              selectedKeys={[this.state.current]}
+              mode="horizontal"
+              style={{ fontSize: '18px', borderBottom: '0' }}
+            >
+              <Item key={NavKeys.SEARCH} style={{ borderBottom: '0' }}>
+                <Icon type="search" />
+              </Item>
+              <Item key={NavKeys.HOME} style={{ borderBottom: '0' }}>
+                <Icon type="home" />
+              </Item>
+              <Item key={NavKeys.LOGIN} style={{ borderBottom: '0' }}>
+                <Icon type="user" />
+              </Item>
+            </Menu>
+          </Col>
+        </Row>
+      </nav>
     );
   }
 }
---
+++ client/src/comp/desktop/nav/logo/Logo.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Link } from 'react-router';
+import Icon from 'comp/icon';
+
+import './_logo.scss';
+
+const Logo = (): JSX.Element => (
+  <Link to="/" className="Logo">
+    <span className="Logo__icon">
+      <Icon type="aliwangwang" />
+    </span>
+    <span className="Logo__name">
+      STRING SYNC
+    </span>
+  </Link>
+);
+
+export default Logo;
