d04371c10c8286337ec820b42a81ff936e62ceba
Jared Johnson <jared@nuts.com>
2017-08-05 14:01:04 -0400

    Add routes for api.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
+  namespace :api, defaults: { format: :json } do
+    namespace :v1 do
+      resources :users,          only: %i(create show update)
+      resources :notations
+      resource  :session,        only: %i(create destroy)
+      resources :user_notations, only: %i(create destroy)
+    end
+  end
+
   root to: "static_pages#root"
   get "*path", to: "static_pages#root"
 end
