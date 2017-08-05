1c543d8527cc0aacc693df1a70b1301e826444f3
Jared Johnson <jared@nuts.com>
2017-08-05 14:03:52 -0400

    Create the notations controller.

---
+++ app/controllers/api/v1/notations_controller.rb
@@ -0,0 +1,36 @@
+class Api::v1::NotationsController < ApplicationController
+  def index
+    @notations = Notation.all
+    render(:index, status: 200)
+  end
+
+  def show
+    @notation = Notation.find(params.require(:id))
+    render(:show, status: 200)
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def destroy
+  end@
+
+  private
+
+    def notation_params
+      params.
+          require(:notation).
+          permit(
+            :id,
+            :video_url,
+            :thumbnail,
+            :name,
+            :artist_name,
+            :build_structs,
+            :scroll_structs
+          )
+    end
+end
