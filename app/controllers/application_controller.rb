69a404c8005ce2a86cc27535089a23ef91d0d91b
Jared Johnson <jared@nuts.com>
2017-08-06 07:29:57 -0400

    Fix viewport scaling on mobile.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,29 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  helper_method :current_user
+  helper_method :logged_in?
+
+  private
+
+    def current_user
+      session_token = session[:session_token]
+      return nil unless session_token
+      @current_user ||= User.find_by(session_token: session_token)
+    end
+
+    def logged_in?
+      !!current_user
+    end
+
+    def login(user)
+      session[:session_token] = user.reset_session_token!
+    end
+
+    def logout
+      return unless current_user
+      current_user.reset_session_token!
+      @current_user = nil
+      session[:session_token] = nil
+    end
 end
