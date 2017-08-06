94d14486f4933dd29b1b1fb3f2e45ab146198446
Jared Johnson <jared@nuts.com>
2017-08-06 08:02:08 -0400

    Add react native to the project.

--- client/src/root/app/layout/mobile/MobileAppLayout.tsx
+++ client/src/root/app/layout/mobile/MobileAppLayout.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
 
-import Tabs from 'antd-mobile/lib/tabs';
-import WhiteSpace from 'antd-mobile/lib/white-space';
-import Badge from 'antd-mobile/lib/badge';
+import { Tabs } from 'antd-mobile';
+
+// import Tabs from 'antd-mobile/lib/tabs';
+// import WhiteSpace from 'antd-mobile/lib/white-space';
+// import Badge from 'antd-mobile/lib/badge';
 
 const MobileAppLayout = ({ location, children }): JSX.Element => (
   <span>Mobile App Layout</span>
