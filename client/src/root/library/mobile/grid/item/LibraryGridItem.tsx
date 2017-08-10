48dfac92c14fc070fa1d2ec84ee931cda29cce0a
Jared Johnson <jared@nuts.com>
2017-08-10 07:59:12 -0400

    Readd react lazy load to mobile library.

--- client/src/root/library/mobile/grid/item/LibraryGridItem.tsx
+++ client/src/root/library/mobile/grid/item/LibraryGridItem.tsx
@@ -1,11 +1,21 @@
 import React from 'react';
 import { Link } from 'react-router';
 
+import LazyLoad from 'react-lazyload';
+
 const LibraryGridItem = ({ data }): JSX.Element => (
   <div style={{ overflow: 'hidden'}}>
     <Link to={data.url} className="LibraryGridItem">
       <div className="LibraryGridItem__imgContainer">
-        <img src={data.thumbnail} className="LibraryGridItem__imgContainer__img" />
+        <LazyLoad
+          once
+          debounce
+          resize
+          height="100%"
+          offset={500}
+        >
+          <img src={data.thumbnail} className="LibraryGridItem__imgContainer__img" />
+        </LazyLoad>
       </div>
     </Link>
   </div>
