f4536fa86f69ee65422faabca07c83dc8d5531ff
Jared Johnson <jared@nuts.com>
2017-08-16 18:50:45 -0400

    Add search bar clear button.

--- client/src/root/search/bar/SearchBar.tsx
+++ client/src/root/search/bar/SearchBar.tsx
@@ -5,17 +5,53 @@ import Icon from 'antd/lib/icon';
 
 const iconStyle = { fontSize: '16px' };
 
-const SearchBar = ({ onSearch, loading }): JSX.Element => (
-  <div className="SearchBar">
-    <Input
-      onChange={(e) => onSearch(e.target.value)}
-      prefix={
-        loading ?
-          <Icon type="loading" style={iconStyle} /> :
-          <Icon type="search"  style={iconStyle} />
-        }
-    />
-  </div>
-);
+interface SearchBarProps {
+  loading: boolean;
+  onSearch: (query: string) => void;
+}
+
+interface SearchBarState {
+  query: string;
+}
+
+class SearchBar extends React.Component<SearchBarProps, SearchBarState> {
+  searchInput: Input;
+  state: SearchBarState = { query: '' };
+
+  emitEmpty = () => {
+    this.searchInput.focus();
+
+    const query = '';
+    this.setState({ query });
+    this.props.onSearch(query);
+  }
+
+  updateQuery = (e: any): void => {
+    const query = e.target.value;
+    this.setState({ query });
+    this.props.onSearch(query);
+  }
+
+  render(): JSX.Element {
+    const { loading } = this.props;
+    const { query } = this.state;
+
+    return (
+      <div className="SearchBar">
+        <Input
+          onChange={this.updateQuery}
+          ref={c => this.searchInput = c}
+          value={query}
+          prefix={
+            loading ?
+              <Icon type="loading" style={iconStyle} /> :
+              <Icon type="search"  style={iconStyle} />
+          }
+          suffix={query ? <Icon type="close-circle" onClick={this.emitEmpty} /> : null}
+          />
+      </div>
+    );
+  }
+}
 
 export default SearchBar;
