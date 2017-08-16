9097c943526f80e3d7c3d8fcf962dcc876fee930
Jared Johnson <jared@nuts.com>
2017-08-15 22:43:59 -0400

    Add tags to the notation detail.

--- client/src/comp/notation/detail/NotationDetail.tsx
+++ client/src/comp/notation/detail/NotationDetail.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Link } from 'react-router';
 
+import Tag from 'antd/lib/tag';
+
 import Card from 'antd/lib/card';
 import LazyLoad from 'react-lazyload';
 
@@ -24,6 +26,11 @@ const NotationDetail = ({ notation }): JSX.Element => (
             <div className="NotationDetail__info">
               <h3>{`${notation.name} by ${notation.artist}`}</h3>
               <p>{notation.transcriber}</p>
+              <div className="NotationDetail__info__tag">
+                {
+                  notation.tags.map(tag => <Tag key={`tag-${tag}-${notation.id}`}>{tag}</Tag>)
+                }
+              </div>
             </div>
           </div>
         </Card>
