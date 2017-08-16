f2800ea19a20f36dc9a92e85a50652f80b7d0329
Jared Johnson <jared@nuts.com>
2017-08-15 22:14:22 -0400

    Increase the debounce time on search.

--- client/src/root/search/Search.tsx
+++ client/src/root/search/Search.tsx
@@ -30,7 +30,7 @@ class Search extends React.Component<SearchProps, SearchState> {
   constructor(props: SearchProps) {
     super(props);
 
-    this.filterNotations = debounce(this._filterNotations, 250);
+    this.filterNotations = debounce(this._filterNotations, 500);
   }
 
   componentWillMount(): void {
