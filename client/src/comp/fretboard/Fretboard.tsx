7f908d65052cc340d31b46b1c8bf0469c017c51e
Jared Johnson <jared@nuts.com>
2017-08-24 06:38:19 -0400

    Remove padding for fretboard.

--- client/src/comp/fretboard/Fretboard.tsx
+++ client/src/comp/fretboard/Fretboard.tsx
@@ -5,8 +5,12 @@ interface FretboardProps {}
 interface FretboardState {}
 
 class Fretboard extends React.Component<FretboardProps, FretboardState> {
-  public render(): JSX.Element {
-    return (<div>Fretboard</div>);
+  render(): JSX.Element {
+    return (
+      <div className="Fretboard">
+        Fretboard
+      </div>
+    );
   }
 }
 
