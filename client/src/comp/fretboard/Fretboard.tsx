7dac28111c8b9c20a3ceae7034aed4810a4ff826
Jared Johnson <jared@nuts.com>
2017-08-26 14:39:04 -0400

    Improve the fretboard component style.

--- client/src/comp/fretboard/Fretboard.tsx
+++ client/src/comp/fretboard/Fretboard.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 
 import Frets from './frets';
 import Strings from './strings';
+import Row from 'antd/lib/row';
+import Col from 'antd/lib/col';
 
 interface FretboardProps {}
 
@@ -11,6 +13,17 @@ class Fretboard extends React.PureComponent<FretboardProps, FretboardState> {
   render(): JSX.Element {
     return (
       <div className="FretboardContainer">
+        <Row type="flex" justify="center" className="Fret__indicator">
+          {
+            Array(23).fill(null).map((_, fret) => (
+              <Col key={`fret-indicator-${fret}`} span={fret === 0 ? 2 : 1}>
+                <Row type="flex" justify="center">
+                  {fret}
+                </Row>
+              </Col>
+            ))
+          }
+        </Row>
         <div className="Fretboard">
           <Frets />
           <Strings />
