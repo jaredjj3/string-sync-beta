574f488dd7de0a2928e8639bd5637b6f57cdcd82
Jared Johnson <jared@nuts.com>
2017-09-07 22:40:03 -0400

    Setup constructor for Vexflow.Player.

--- client/src/services/vexflow/player/Player.ts
+++ client/src/services/vexflow/player/Player.ts
@@ -1,5 +1,13 @@
-class Player {
+import { VideoPlayer } from 'types/videoPlayer';
+import Artist from '../artist';
 
+class Player {
+  videoPlayer: VideoPlayer = null;
+  artist: Artist = null;
+
+  constructor(videoPlayer: VideoPlayer) {
+    this.videoPlayer = videoPlayer;
+  }
 }
 
 export default Player;
---
+++ client/src/types/videoPlayer.d.ts
@@ -0,0 +1,5 @@
+interface VideoPlayer {
+  getCurrentTime(): number;
+}
+
+export { VideoPlayer };
