9e7bca156402f6334907c3a0091c4cadbb9c521b
Jared Johnson <jared@nuts.com>
2017-09-18 20:56:23 -0400

    Implement the play pause button.

--- client/src/comp/tabVideoControls/play/Play.tsx
+++ client/src/comp/tabVideoControls/play/Play.tsx
@@ -6,24 +6,34 @@ import Icon from 'antd/lib/icon';
 
 interface PlayProps {
   isMobile: boolean;
+  isVideoActive: boolean;
+  videoPlayer: any;
 }
 
 interface PlayState {}
 
 class Play extends React.Component<PlayProps, PlayState> {
+  pauseVideo = (): void => {
+    this.props.videoPlayer.pauseVideo();
+  }
+
+  playVideo = (): void => {
+    this.props.videoPlayer.playVideo();
+  }
+
   render(): JSX.Element {
-    const { isMobile } = this.props;
+    const { isMobile, isVideoActive } = this.props;
 
     return (
       <Row type="flex">
         <span>
           <Row type="flex" align="middle" justify="center">
-            <Icon type="play-circle-o" />
             {
-              isMobile ?
-                null :
-                <span style={{ marginLeft: '5px' }}>{`${0.0}s`}</span>
+              isVideoActive ?
+                <Icon type="pause-circle-o" onClick={this.pauseVideo} /> :
+                <Icon type="play-circle-o" onClick={this.playVideo} />
             }
+            {isMobile ? null : <span style={{ marginLeft: '5px' }}>{`${0.0}s`}</span>}
           </Row>
         </span>
         {
@@ -41,8 +51,12 @@ class Play extends React.Component<PlayProps, PlayState> {
   }
 }
 
+import { isVideoActive } from 'util/videoStateCategory';
+
 const mapStateToProps = state => ({
-  isMobile: state.device.type === 'MOBILE' || state.device.isTouch
+  isMobile: state.device.type === 'MOBILE' || state.device.isTouch,
+  isVideoActive: isVideoActive(state.video.state),
+  videoPlayer: state.video.player
 });
 
 const mapDispatchToProps = dispatch => ({
