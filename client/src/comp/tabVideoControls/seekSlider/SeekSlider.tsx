f9bcfbfe953d9875f871a1a840896ae9cedafddb
Jared Johnson <jared@nuts.com>
2017-09-19 05:36:20 -0400

    Implement the SeekSlider.

--- client/src/comp/tabVideoControls/seekSlider/SeekSlider.tsx
+++ client/src/comp/tabVideoControls/seekSlider/SeekSlider.tsx
@@ -3,22 +3,157 @@ import { connect } from 'react-redux';
 
 import Slider from 'antd/lib/slider';
 
-interface SeekSliderProps {}
+import { throttle } from 'lodash';
+import interpolator from 'util/interpolator';
+import formatTime from 'util/formatTime';
 
-interface SeekSliderState {}
+import { Interpolator } from 'util/interpolator';
+import { VideoPlayer } from 'types/videoPlayer';
+
+interface SeekSliderProps {
+  shouldRAF: boolean;
+  videoPlayer: VideoPlayer;
+  isVideoActive: boolean;
+  duration: number
+}
+
+interface SeekSliderState {
+  seekSliderValue: number;
+}
 
 class SeekSlider extends React.PureComponent<SeekSliderProps, SeekSliderState> {
+  RAFHandle: number = null;
+  isScrubbing: boolean = false;
+  shouldPlayOnScrubEnd: boolean = false;
+  maybeSeekVideo: any = null;
+  handleChange: any = null;
+  state: SeekSliderState = {
+    seekSliderValue: 0
+  };
+
+  private _toTime: Interpolator = null;
+  private _toSeekSliderValue: Interpolator = null;
+
+  constructor(props: SeekSliderProps) {
+    super(props);
+
+    this.handleChange = throttle(this._handleChange, 10);
+    this.maybeSeekVideo = throttle(this._maybeSeekVideo, 250);
+  }
+
+  componentWillReceiveProps(nextProps: SeekSliderProps): void {
+    this._toTime = interpolator({ x: 0, y: 0 }, { x: 100, y: nextProps.duration });
+    this._toSeekSliderValue = interpolator({ x: 0, y: 0 }, { x: nextProps.duration, y: 100 });
+
+    this.updateSeekSliderValue();
+  }
+
+  shouldComponentUpdate(nextProps: SeekSliderProps, nextState: SeekSliderState): boolean {
+    return (
+      nextProps.shouldRAF ||
+      this.state.seekSliderValue !== nextState.seekSliderValue
+    );
+  }
+
+  componentDidUpdate(): void {
+    if (this.props.shouldRAF) {
+      this.RAFHandle = window.requestAnimationFrame(this.updateSeekSliderValue);
+    } else {
+      window.cancelAnimationFrame(this.RAFHandle);
+      this.RAFHandle = null;
+    }
+  }
+
+  get areConvertorFuncsSet(): boolean {
+    return this._toSeekSliderValue !== null && this._toTime !== null;
+  }
+
+  updateSeekSliderValue = (): void => {
+    if (!this.props.videoPlayer || !this.areConvertorFuncsSet) {
+      return;
+    }
+
+    const seekSliderValue = this._toSeekSliderValue(this.props.videoPlayer.getCurrentTime()) || 0;
+    this.setState(Object.assign({}, this.state, { seekSliderValue }));
+  }
+
+  handleAfterChange = (): void => {
+    const { videoPlayer } = this.props;
+
+    if (this.shouldPlayOnScrubEnd) {
+      videoPlayer.playVideo();
+    } else {
+      videoPlayer.pauseVideo();
+    }
+
+    this.shouldPlayOnScrubEnd = false;
+    this.isScrubbing = false;
+  }
+
+  tipFormatter = (value: number): string => {
+    if (this._toTime === null) {
+      return '0.0';
+    }
+
+    return formatTime(this._toTime(value));
+  }
+
   render(): JSX.Element {
+    const { seekSliderValue } = this.state;
+
     return (
       <div>
-        <Slider />
+        <Slider
+          step={0.01}
+          value={seekSliderValue}
+          defaultValue={0}
+          onChange={this.handleChange}
+          onAfterChange={this.handleAfterChange}
+          tipFormatter={this.tipFormatter}
+        />
       </div>
     );
   }
+
+  private _handleChange = (seekSliderValue: number): void => {
+    this._maybeStartScrubbing();
+    this.maybeSeekVideo(this.state.seekSliderValue, seekSliderValue); // throttled
+    this.setState(Object.assign({}, this.state, { seekSliderValue }));
+  }
+
+  private _maybeStartScrubbing = (): void => {
+    const { videoPlayer, isVideoActive } = this.props;
+
+    if (this.isScrubbing) {
+      return;
+    }
+
+    this.shouldPlayOnScrubEnd = isVideoActive;
+    this.isScrubbing = true;
+
+    videoPlayer.pauseVideo();
+  }
+
+  private _maybeSeekVideo = (oldValue: number, newValue: number): void => {
+    if (!this.isScrubbing) {
+      return;
+    }
+
+    const { videoPlayer } = this.props;
+
+    if (oldValue !== newValue) {
+      videoPlayer.seekTo(this._toTime(newValue));
+    }
+  }
 }
 
-const mapStateToProps = state => ({
+import { isVideoActive } from 'util/videoStateCategory';
 
+const mapStateToProps = state => ({
+  shouldRAF: state.video.player && isVideoActive(state.video.state),
+  isVideoActive: isVideoActive(state.video.state),
+  videoPlayer: state.video.player,
+  duration: state.notation.duration / 1000
 });
 
 const mapDispatchToProps = dispatch => ({
--- client/src/types/videoPlayer.d.ts
+++ client/src/types/videoPlayer.d.ts
@@ -1,6 +1,9 @@
 interface VideoPlayer {
   getCurrentTime(): number;
   setPlaybackRate(playbackRate: number): VideoPlayer;
+  seekTo(time: number): VideoPlayer;
+  playVideo(): VideoPlayer;
+  pauseVideo(): VideoPlayer;
 }
 
 export { VideoPlayer };
