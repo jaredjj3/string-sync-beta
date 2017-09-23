c03a41383299243ce2afc6a480dcc9a207cac666
Jared Johnson <jared@nuts.com>
2017-09-23 12:15:42 -0400

    Update the store tuning from the tuning component.

--- client/src/data/tuning/reducer.ts
+++ client/src/data/tuning/reducer.ts
@@ -1,21 +1,14 @@
 import { UPDATE_TUNING, RESET_TUNING } from './actions';
 
-interface StoreTuning {
-  tuning: string;
-}
-
-const defaultState: StoreTuning = Object.freeze({
-  tuning: 'EADBGE'
-});
+const defaultState: string = 'EADGBE';
 
 export default (state = defaultState, action) => {
   Object.freeze(state);
-  const nextState = Object.assign({}, state);
+  const nextState = state.repeat(1);
 
   switch (action.type) {
     case UPDATE_TUNING:
-      nextState.tuning = action.tuning;
-      return nextState;
+      return action.tuning.repeat(1);
 
     case RESET_TUNING:
       return defaultState;
--- client/src/root/notation/edit/tuning/Tuning.tsx
+++ client/src/root/notation/edit/tuning/Tuning.tsx
@@ -3,29 +3,51 @@ import { connect } from 'react-redux';
 
 import Input from 'antd/lib/input';
 
-interface TuningProps {}
+interface TuningProps {
+  tuning: string;
+  updateTuning(tuning: string): void;
+  resetTuning(): void;
+}
 
 interface TuningState {}
 
 class Tuning extends React.Component<TuningProps, TuningState> {
+  componentWillUnmount(): void {
+    this.props.resetTuning();
+  }
+
+  handleChange = (e: any): void => {
+    this.props.updateTuning(e.target.value);
+  }
+
   render(): JSX.Element {
+    const { tuning } = this.props;
+
     return (
       <div className="TuningContainer">
         <div className="Tuning">
           <h2>Tuning</h2>
-          <Input size="large" style={{ marginTop: '10px' }} />
+          <Input
+            size="large"
+            style={{ marginTop: '10px' }}
+            value={tuning}
+            onChange={this.handleChange}
+          />
         </div>
       </div>
     );
   }
 }
 
-const mapStateToProps = state => ({
+import { updateTuning, resetTuning } from 'data/tuning/actions';
 
+const mapStateToProps = state => ({
+  tuning: state.tuning
 });
 
 const mapDispatchToProps = dispatch => ({
-
+  updateTuning: (tuning: string) => dispatch(updateTuning(tuning)),
+  resetTuning: () => dispatch(resetTuning())
 });
 
 export default connect(
