e340d82c41495d9881cfcbd3d5bd5f715ca54c6c
Jared Johnson <jared@nuts.com>
2017-10-23 14:52:32 -0400

    Calculate the device viewport in the preloaded state.

--- client/src/data/store.ts
+++ client/src/data/store.ts
@@ -3,8 +3,19 @@ import { createStore } from 'redux';
 import reducer from './reducer';
 import middleware from './middleware';
 
+const isTouch = /iPhone|iPad|iPod|Android|Vita/i.test(navigator.userAgent);
+const width = window.innerWidth;
+const height = window.innerHeight;
+const orientation = width > height ? 'LANDSCAPE' : 'PORTRAIT';
+const type = (width < 992 || isTouch) ? 'MOBILE' : 'DESKTOP';
+
 const preLoadedState = {
-  session: { currentUser: window.currentUser }
+  session: { currentUser: window.currentUser },
+  device: {
+    viewport: { height, width, orientation },
+    type,
+    isTouch
+  }
 };
 
 const store = createStore(
