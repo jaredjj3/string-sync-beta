77a8af79309fe82766bfb7f2b1dff680e718a7bf
Jared Johnson <jared@nuts.com>
2017-11-08 20:18:53 -0500

    Conditionally render logged in component on the desktop landing.

--- client/src/root/routes/landing/desktop/DesktopLanding.tsx
+++ client/src/root/routes/landing/desktop/DesktopLanding.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
-import { compose, lifecycle } from 'recompose';
+import { compose, lifecycle, branch, renderNothing } from 'recompose';
 
 import Youtube from 'react-youtube';
 import Logo from 'comp/logo';
 import { Row, Col, Button } from 'antd';
 import { enableFeatures, disableFeatures } from 'data/feature/actions';
+import { identity } from 'enhancers';
 
 const youtubeOptions = {
   playerVars: {
@@ -22,7 +23,36 @@ const youtubeOptions = {
   }
 };
 
-const DesktopLanding = () => (
+const mapIsLoggedInToProps = state => ({
+  isLoggedIn: Boolean(state.session.currentUser.id)
+});
+
+const showIfLoggedOut = (Component: any) => (
+  compose(
+    connect(mapIsLoggedInToProps, null),
+    branch(({ isLoggedIn }) => !isLoggedIn, identity, renderNothing),
+  )(Component)
+);
+
+const LoginLink = showIfLoggedOut(() => (
+  <li>
+    <Link to="/login">
+      login
+    </Link>
+  </li>
+));
+
+const SignupButton = showIfLoggedOut(() => (
+  <span>
+    <Button size="large">
+      <Link to="/signup">
+        Join StringSync!
+      </Link>
+    </Button>
+  </span>
+));
+
+const DesktopLanding = ({ isLoggedIn }) => (
   <div className="Landing--desktop">
     <div className="Landing--desktop__altActionBar">
       <ul className="AltActionBar__links">
@@ -41,11 +71,7 @@ const DesktopLanding = () => (
             library
           </Link>
         </li>
-        <li>
-          <Link to="/login">
-            login
-          </Link>
-        </li>
+        <LoginLink />
       </ul>
     </div>
     <section>
@@ -60,13 +86,7 @@ const DesktopLanding = () => (
             </Link>
           </Button>
         </span>
-        <span>
-          <Button size="large">
-            <Link to="/signup">
-              Join StringSync!
-            </Link>
-          </Button>
-        </span>
+        <SignupButton />
       </div>
     </section>
     <section>
@@ -112,61 +132,3 @@ export default compose(
     }
   })
 )(DesktopLanding);
-
-// const Landing = () => (
-//   <div className="Home">
-//     <div className="JumboTron">
-//       <figure className="JumboTron">
-//         <div className="JumboTron__image">
-//           Jumbotron Image
-//         </div>
-//         <figcaption className="JumboTron__caption">
-//           <h1>StringSync is easy. Promise.</h1>
-//           <h2>Built for you</h2>
-//         </figcaption>
-//         <div>
-//           <ul>
-//             <li>img1</li>
-//             <li>img2</li>
-//             <li>img3</li>
-//           </ul>
-//         </div>
-//       </figure>
-//       <div className="JumboTron__signupContainer">
-//         <Signup />
-//       </div>
-//     </div>
-//     <div className="Home__about">
-//       <h2>Traditional Guitar Teaching Methods are Inefficient</h2>
-//       <p>
-//         You look at a tab and pause. Which string? Which finger?
-//         It's inefficient.
-//       </p>
-//       <Button size="large">
-//         <Link to="/library">
-//           Find out more
-//         </Link>
-//       </Button>
-
-//       <h2>StringSync Thinks for You</h2>
-//       <p>
-//         StringSync provides an intuitive interface for learning.
-//         Plain and simple.
-//       </p>
-//       <Button size="large">
-//         <Link to="/library">
-//           Go to a Random Video
-//         </Link>
-//       </Button>
-
-//       <h2>What are you Waiting For?!</h2>
-//       <Button size="large">
-//         <Link to="/library">
-//           Start Learning Now!
-//         </Link>
-//       </Button>
-//     </div>
-//   </div>
-// );
-
-// export default Landing;
