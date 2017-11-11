41b236c62f63d98833c4f78f2690e936863bebcf
Jared Johnson <jared@nuts.com>
2017-11-11 07:14:20 -0500

    Add note style static members to the VexPlayer service.

--- client/src/services/vexflow/player/VexPlayer.ts
+++ client/src/services/vexflow/player/VexPlayer.ts
@@ -1,5 +1,13 @@
 class VexPlayer {
+  static DEFAULT_NOTE_STYLE: any = {
+    fillStyle: '#000000',
+    strokeStyle: '#000000'
+  };
 
+  static PRESSED_NOTE_STYLE: any = {
+    fillStyle: '#FF0000',
+    strokeStyle: '#FF0000'
+  };
 }
 
 export default VexPlayer;
