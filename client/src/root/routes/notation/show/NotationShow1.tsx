7c6046408f0f6f0421e39f63dc259161b6532dc9
Jared Johnson <jared@nuts.com>
2017-11-18 19:18:53 -0500

    Recompose the notation show component.

--- client/src/root/routes/notation/show/NotationShow1.tsx
+++ client/src/root/routes/notation/show/NotationShow1.tsx
@@ -1,61 +1,12 @@
 import React from 'react';
 import { withRouter } from 'react-router-dom';
 import { connect } from 'react-redux';
-import { compose } from 'recompose';
+import { compose, lifecycle } from 'recompose';
 
 import Banner from './banner';
 import { withRAFLoop, withVideo } from 'enhancers';
-
-interface NotationShowProps {
-  match: any;
-  RAFLoop: any;
-  shouldRAF: boolean;
-  fetchNotation(notationId: number): void;
-  resetNotation(): void;
-  showNav(): void;
-  hideNav(): void;
-  resetRAFLoop(): void;
-}
-
-interface NotationShowState {}
-
-class NotationShow extends React.Component<NotationShowProps, NotationShowState> {
-  componentDidMount(): void {
-    const notationId = this.props.match.params.id;
-    this.props.fetchNotation(notationId);
-  }
-
-  componentWillReceiveProps(nextProps: NotationShowProps): void {
-    const { RAFLoop, shouldRAF } = nextProps;
-
-    if (shouldRAF) {
-      RAFLoop.start();
-    } else {
-      RAFLoop.stop();
-    }
-  }
-
-  componentWillUnmount(): void {
-    this.props.resetNotation();
-    this.props.showNav();
-    this.props.resetRAFLoop();
-  }
-
-  render(): JSX.Element {
-    return (
-      <div className="NotationShow">
-        <Banner />
-      </div>
-    );
-  }
-}
-
 import { fetchNotation, resetNotation } from 'data/notation/actions';
 import { enableFeatures, disableFeatures } from 'data/feature/actions';
-
-const mapStateToProps = state => ({
-
-});
 
 const mapDispatchToProps = dispatch => ({
   fetchNotation: (notationId: number) => dispatch(fetchNotation(notationId)),
@@ -64,8 +15,30 @@ const mapDispatchToProps = dispatch => ({
   hideNav: () => dispatch(disableFeatures(['navbar']))
 });
 
-export default compose(
+const enhance = compose(
   withRAFLoop,
   withVideo,
-  connect(mapStateToProps, mapDispatchToProps)
-)(NotationShow);
+  connect(null, mapDispatchToProps),
+  lifecycle({
+    componentDidMount(): void {
+      const notationId = this.props.match.params.id;
+      this.props.fetchNotation(notationId);
+      this.props.hideNav();
+    },
+    componentWillReceiveProps(nextProps: any): void {
+      const { RAFLoop, isVideoActive } = nextProps;
+      isVideoActive ? RAFLoop.start() : RAFLoop.stop();
+    },
+    componentWillUnmount(): void {
+      this.props.resetNotation();
+      this.props.showNav();
+      this.props.resetRAFLoop();
+    }
+  })
+);
+
+export default enhance(() => (
+  <div className="NotationShow">
+    <Banner />
+  </div>
+));
