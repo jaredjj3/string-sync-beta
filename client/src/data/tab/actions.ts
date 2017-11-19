465e341349ee2c07efde88219fd9a3558943acc3
Jared Johnson <jared@nuts.com>
2017-11-19 08:23:48 -0500

    Add the emitUpdate and resetTab actions.

--- client/src/data/tab/actions.ts
+++ client/src/data/tab/actions.ts
@@ -1,6 +1,16 @@
 export const SET_PROVIDER = 'SET_PROVIDER';
+export const EMIT_UPDATE = 'EMIT_UPDATE';
+export const RESET_TAB = 'RESET_TAB';
 
 export const setProvider = (provider: any) => ({
   type: SET_PROVIDER,
   provider
+});
+
+export const emitUpdate = () => ({
+  type: EMIT_UPDATE
+});
+
+export const resetTab = () => ({
+  type: RESET_TAB
 });
--- client/src/data/tab/reducer.ts
+++ client/src/data/tab/reducer.ts
@@ -1,23 +1,35 @@
-import { SET_PROVIDER } from './actions';
+import { SET_PROVIDER, EMIT_UPDATE, RESET_TAB } from './actions';
 
 const defaultState = Object.freeze({
-  provider: null
+  provider: null,
+  updatedAt: null
 });
 
 const dup = (state: any): any => Object.assign({}, state);
 
 const TabReducer = (state = defaultState, action) => {
   Object.freeze(state);
-  const nextState = dup(state);
+  let nextState = dup(state);
 
   switch (action.type) {
     case SET_PROVIDER:
       nextState.provider = action.provider;
-      return nextState;
+      break;
+
+    // effectively updates the updatedAt
+    case EMIT_UPDATE:
+      break;
+
+    case RESET_TAB:
+      nextState = dup(defaultState);
+      break;
 
     default:
       return nextState;
   }
+
+  nextState.updatedAt = Date.now();
+  return nextState;
 };
 
 export default TabReducer;
