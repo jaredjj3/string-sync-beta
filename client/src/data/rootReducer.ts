67f545b22bad01edca3ee23a10dc7decc10741e7
Jared Johnson <jared@nuts.com>
2017-12-01 08:50:17 -0500

    Add helpful comments to the root reducer.

--- client/src/data/rootReducer.ts
+++ client/src/data/rootReducer.ts
@@ -1,5 +1,8 @@
 import { combineReducers } from 'redux';
 
+// The API reducers correspond nearly 1-to-1 with the StringSync backend API.
+// Even though the data are used in the UI, these reducers are classified as
+// API because they couple with some external service.
 import { notationReducer as notation } from './api/notation';
 import { notationsReducer as notations } from './api/notations';
 import { sessionReducer as session } from './api/session';
@@ -7,6 +10,9 @@ import { tagsReducer as tags } from './api/tags';
 import { userNotationReducer as userNotation } from './api/userNotation';
 import { userReducer as user } from './api/user';
 
+// The UI reducers manage data that is purely concerned with the client side.
+// However, data managed here may originate from an API, but these reducers
+// must not be the "first pass" managers of such data.
 import { behaviorsReducer as behaviors } from './ui/behaviors';
 import { featuresReducer as features } from './ui/features';
 import { rafReducer as raf } from './ui/raf';
