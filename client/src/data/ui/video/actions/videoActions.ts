7abca385c0e4081b5f3a8d63f0f527d8abfec001
Jared Johnson <jared@nuts.com>
2017-12-02 09:35:16 -0500

    Use "set" instead of "update" in the videoActions.

--- client/src/data/ui/video/actions/videoActions.ts
+++ client/src/data/ui/video/actions/videoActions.ts
@@ -1,20 +1,20 @@
-export const UPDATE_VIDEO_PLAYER = 'UPDATE_VIDEO_PLAYER';
-export const UPDATE_VIDEO_STATE = 'UPDATE_VIDEO_STATE';
-export const UPDATE_VIDEO_LOOP = 'UPDATE_VIDEO_LOOP';
+export const SET_VIDEO_PLAYER = 'SET_VIDEO_PLAYER';
+export const SET_VIDEO_STATE = 'SET_VIDEO_STATE';
+export const SET_VIDEO_LOOP = 'SET_VIDEO_LOOP';
 export const RESET_VIDEO = 'RESET_VIDEO';
 
-export const updateVideoPlayer = videoPlayer => ({
-  type: UPDATE_VIDEO_PLAYER,
+export const setVideoPlayer = videoPlayer => ({
+  type: SET_VIDEO_PLAYER,
   videoPlayer
 });
 
-export const updateVideoState = videoState => ({
-  type: UPDATE_VIDEO_STATE,
+export const setVideoState = videoState => ({
+  type: SET_VIDEO_STATE,
   videoState
 });
 
-export const updateLoop = loop => ({
-  type: UPDATE_VIDEO_LOOP,
+export const setVideoLoop = (loop: [number, number]) => ({
+  type: SET_VIDEO_LOOP,
   loop
 });
 
