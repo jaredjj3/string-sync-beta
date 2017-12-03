d01d73eebc7a82d7a0306c2020ec4601639b9500
Jared Johnson <jared@nuts.com>
2017-12-03 14:02:55 -0500

    Fix the play button.

--- client/src/components/video/controls/play/Play.tsx
+++ client/src/components/video/controls/play/Play.tsx
@@ -7,22 +7,22 @@ import { withVideo } from 'enhancers';
 const playVideo = (videoPlayer) => () => videoPlayer.playVideo();
 const pauseVideo = (videoPlayer) => () => videoPlayer.pauseVideo();
 
-const Play = ({ videoPlayer }) => (
+const Play = ({ video }) => (
   <span>
-    <Icon type="play-circle-o" onClick={playVideo(videoPlayer)} />
+    <Icon type="play-circle-o" onClick={playVideo(video.player)} />
   </span>
 );
 
-const Pause = ({ videoPlayer }) => (
+const Pause = ({ video }) => (
   <span>
-    <Icon type="pause-circle-o" onClick={pauseVideo(videoPlayer)} />
+    <Icon type="pause-circle-o" onClick={pauseVideo(video.player)} />
   </span>
 );
 
 export default compose(
   withVideo,
   branch(
-    ({ isVideoActive }) => isVideoActive,
+    ({ video }) => video.isVideoActive,
     renderComponent(Pause),
     renderComponent(Play)
   ),
