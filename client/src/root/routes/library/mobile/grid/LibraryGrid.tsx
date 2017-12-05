d6eaa1befa06165b12e5f101f108d431f5af52f1
Jared Johnson <jared@nuts.com>
2017-12-05 07:28:51 -0500

    Fix antd-mobile import in the LibraryGrid component.

--- client/src/root/routes/library/mobile/grid/LibraryGrid.tsx
+++ client/src/root/routes/library/mobile/grid/LibraryGrid.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-import Grid from 'antd-mobile/lib/grid';
+import { Grid } from 'antd-mobile';
 import LibraryGridItem from './item';
 
 import { TagNotations } from '../../Library';
