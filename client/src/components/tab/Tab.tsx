89a8cb2c2cbb904c90371c813484c258e4042b89
Jared Johnson <jared@nuts.com>
2017-12-07 18:40:51 -0500

    Comment out scoreContainer classnames to test on mobile.

--- client/src/components/tab/Tab.tsx
+++ client/src/components/tab/Tab.tsx
@@ -8,10 +8,11 @@ import classNames from 'classnames';
 const Tab = (props) => {
   const scoreContainerClassNames = classNames(
     'ScoreContainer',
-    {
-      'ScoreContainer__noScroll': !props.allowScroll,
-      'ScoreContainer__scroll': props.allowScroll
-    }
+    'ScoreContainer__scroll'
+    // {
+    //   'ScoreContainer__noScroll': !props.allowScroll,
+    //   'ScoreContainer__scroll': props.allowScroll
+    // }
   );
 
   return (
