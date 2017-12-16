d7d2549d9481f4f7fe3959b2c5f0e8c74c96545f
Jared Johnson <jared@nuts.com>
2017-12-16 08:07:53 -0500

    Recompose the gradient component to render only when its features is true.

--- client/src/components/gradient/Gradient.tsx
+++ client/src/components/gradient/Gradient.tsx
@@ -1,7 +1,18 @@
 import React from 'react';
 
 import './_gradient.less';
+import { compose, branch, renderNothing } from 'recompose';
+import { identity, withFeatures } from 'enhancers';
 
 const Gradient = (): JSX.Element => <div className="Gradient" />;
 
-export default Gradient;
+const enhance = compose(
+  withFeatures,
+  branch(
+    ({ features }) => features.gradient,
+    identity,
+    renderNothing
+  )
+);
+
+export default enhance(Gradient);
