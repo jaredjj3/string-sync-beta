3671b7a035f0f8ff7efeb0b64f326f44cbb98088
Jared Johnson <jared@nuts.com>
2017-12-17 12:51:46 -0500

    Make the boilerplate work.

--- webpack.config.js
+++ webpack.config.js
@@ -1,94 +1,114 @@
-const path = require('path');
-const autoprefixer = require('autoprefixer');
-const webpack = require('webpack');
-const fs = require('fs');
+const Webpack = require('webpack');
+const Path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
-const lessToJs = require('less-vars-to-js');
-const themeVariables = lessToJs(
-  fs.readFileSync(path.join(__dirname, 'client', 'src', 'styles', 'vars', 'overrides.less'), 'utf8')
-);
-const svgDirs = [
-  require.resolve('antd-mobile').replace(/warn\.js$/, '')
-]
+const isProduction = process.argv.indexOf('-p') >= 0;
+const outPath = Path.join(__dirname, 'app', 'assets', 'javascripts');
+const sourcePath = Path.join(__dirname, 'client', 'src');
 
 module.exports = {
-  context: __dirname,
-  entry: './client/src/stringSync.tsx',
-  devtool: 'source-map',
-  resolve: {
-    modules: [path.resolve(__dirname, 'client', 'src'), 'node_modules'],
-    extensions: [
-      '.web.tsx', '.web.ts', '.web.jsx', '.web.js', '.ts', '.tsx', 'd.ts',
-      '.js',
-      '.jsx',
-      '.react.js'
+  context: sourcePath,
+  entry: {
+    main: './index.tsx',
+    vendor: [
+      'react',
+      'react-dom',
+      'react-redux',
+      'react-router',
+      'redux'
     ]
   },
   output: {
-    path: path.join(__dirname, 'app', 'assets', 'javascripts'),
-    publicPath: '/assets/',
+    path: outPath,
+    publicPath: '/',
     filename: 'bundle.js',
-    hotUpdateChunkFilename: 'hot-update.js',
-    hotUpdateMainFilename: 'hot-update.json',
-    devtoolModuleFilenameTemplate: '[resourcePath]',
-    devtoolFallbackModuleFilenameTemplate: '[resourcePath]?[hash]'
   },
-  plugins: [
-    new webpack.HotModuleReplacementPlugin()
-  ],
-  devServer: {
-    hot: true,
-    contentBase: path.join(__dirname, 'client', 'src'),
-    proxy: {
-      '**' : 'http://localhost:3000'
-    },
-    headers: { 
-      "X-Custom-Header": "yes"
-    },
-    stats: { 
-      colors: true 
-    }, 
-    inline: true
+  target: 'web',
+  resolve: {
+    extensions: ['.js', '.ts', '.tsx'],
+    // Fix webpack's default behavior to not load packages with jsnext:main module
+    // https://github.com/Microsoft/TypeScript/issues/11677
+    mainFields: ['browser', 'main']
   },
   module: {
     loaders: [
-       {
-         test: /\.(svg)$/i,
-         loader: 'svg-sprite-loader',
-         include: svgDirs
-       }, {
-        test: /\.less$/,
-        use: [
-          { loader: "style-loader" },
-          { loader: "css-loader" },
-          { 
-            loader: "less-loader",
-            options: {
-              modifyVars: themeVariables,
-              paths: [
-                path.resolve(__dirname, 'client', 'src', 'styles'),
-                path.resolve(__dirname, 'node_modules')
-              ]
+      // .ts, .tsx
+      {
+        test: /\.tsx?$/,
+        use: isProduction
+          ? 'awesome-typescript-loader?module=es6'
+          : [
+            'react-hot-loader/webpack',
+            'awesome-typescript-loader'
+          ]
+      },
+      // css
+      {
+        test: /\.css$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: [
+            {
+              loader: 'css-loader',
+              query: {
+                modules: true,
+                sourceMap: !isProduction,
+                importLoaders: 1,
+                localIdentName: '[local]__[hash:base64:5]'
+              }
+            },
+            {
+              loader: 'postcss-loader',
+              options: {
+                ident: 'postcss',
+                plugins: [
+                  require('postcss-import')({ addDependencyTo: Webpack }),
+                  require('postcss-url')(),
+                  require('postcss-cssnext')(),
+                  require('postcss-reporter')(),
+                  require('postcss-browser-reporter')({ disabled: isProduction }),
+                ]
+              }
             }
-          },
-        ]
-      }, {
-        test: [/\.jsx?$/, /\.js?$/],
-        exclude: /node_modules/,
-        loader: 'babel-loader',
-      }, {
-        test: [/\.tsx?$/],
-        use: [
-          { 
-            loader: 'babel-loader',
-          }, { 
-            loader: 'ts-loader',
-            query: {
-              transpileOnly: true
-            }
-          }
-        ]
-      }
-    ]
+          ]
+        })
+      },
+      // static assets
+      { test: /\.html$/, use: 'html-loader' },
+      { test: /\.png$/, use: 'url-loader?limit=10000' },
+      { test: /\.jpg$/, use: 'file-loader' },
+    ],
+  },
+  plugins: [
+    new Webpack.DefinePlugin({
+      'process.env.NODE_ENV': isProduction === true ? JSON.stringify('production') : JSON.stringify('development')
+    }),
+    new Webpack.optimize.CommonsChunkPlugin({
+      name: 'vendor',
+      filename: 'vendor.bundle.js',
+      minChunks: Infinity
+    }),
+    new Webpack.optimize.AggressiveMergingPlugin(),
+    new ExtractTextPlugin({
+      filename: 'styles.css',
+      disable: !isProduction
+    }),
+    new HtmlWebpackPlugin({
+      template: 'index.html'
+    })
+  ],
+  devServer: {
+    contentBase: sourcePath,
+    hot: true,
+    stats: {
+      warnings: false
+    },
+  },
+  node: {
+    // workaround for webpack-dev-server issue
+    // https://github.com/webpack/webpack-dev-server/issues/60#issuecomment-103411179
+    fs: 'empty',
+    net: 'empty'
   }
-}
+};
