0de27b76a33efcbc0af178b167561fb26906cd2f
Jared Johnson <jared@nuts.com>
2017-12-18 08:16:01 -0500

    Add the withNotation enhancer.

---
+++ client/src/enhancers/withNotation.ts
@@ -0,0 +1,31 @@
+import { connect } from 'react-redux';
+import { notationActions as actions } from 'data/api/notation';
+
+const mapStateToProps = ({ notation }) => ({
+  notation
+});
+
+const mapDispatchToProps = dispatch => ({
+  setNotation: notation => dispatch(actions.setNotation(notation)),
+    resetNotation: () => dispatch(actions.resetNotation),
+    fetchNotation: notationId => dispatch(actions.fetchNotation(notationId)),
+    createNotation: notation => dispatch(actions.createNotation(notation)),
+    updateNotation: notation => dispatch(actions.updateNotation(notation)),
+    destroyNotation: notationId => dispatch(actions.destroyNotation(notationId))
+});
+
+const mergeProps = (stateProps, dispatchProps, ownProps) => ({
+  ...ownProps,
+  notation: {
+    state: stateProps.notation,
+    dispatch: dispatchProps
+  }
+});
+
+const withViewport = Component => connect(
+  mapStateToProps,
+  mapDispatchToProps,
+  mergeProps
+)(Component);
+
+export default withViewport;
