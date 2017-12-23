8bea50b7347dff91839b8fcc56fa74b68c110458
Jared Johnson <jared@nuts.com>
2017-12-23 14:51:48 -0500

    Set the skeleton of the NotationNew component.

--- client/src/components/notation/new/NotationNew.tsx
+++ client/src/components/notation/new/NotationNew.tsx
@@ -1,9 +1,77 @@
 import * as React from 'react';
+import { compose, withState, withProps, withHandlers } from 'recompose';
+import { Form, Button, Select } from 'antd';
+import { Gradient, Nav } from 'components';
 
-const Upload = () => (
-  <div>
-    Upload
+const { Item } = Form;
+const { Option } = Select;
+
+const formItemLayout = {
+  labelCol: {
+    xs: { span: 24 },
+    sm: { span: 24 },
+  },
+  wrapperCol: {
+    xs: { span: 24 },
+    sm: { span: 24 },
+  },
+};
+
+const enhance = compose(
+  Form.create(),
+);
+
+const NotationNew = ({ form, loading, handleFileChange, handleSubmit }) => (
+  <div className="NotationNew">
+    <Gradient />
+    <Nav />
+    <div className="Form">
+      <h1 className="Form__title">UPLOAD</h1>
+      <Form onSubmit={handleSubmit}>
+        <Item
+          hasFeedback
+          label="YouTube URL"
+          {...formItemLayout}
+        >
+          {YoutubeVideoIdInput(form)}
+        </Item>
+        <Item
+          hasFeedback
+          label="Song Name"
+          {...formItemLayout}
+        >
+          {SongNameInput(form)}
+        </Item>
+        <Item
+          hasFeedback
+          label="Artist Name"
+          {...formItemLayout}
+        >
+          {ArtistNameInput(form)}
+        </Item>
+        <Item
+          hasFeedback
+          label="Tags"
+          {...formItemLayout}
+        >
+          {TagsInput(form)}
+        </Item>
+        <Item>
+          {FileInput(form, handleFileChange)}
+        </Item>
+        <Item>
+          <Button
+            type="primary"
+            htmlType="submit"
+            className="Form__submit"
+            loading={loading}
+          >
+            Upload
+          </Button>
+        </Item>
+      </Form>
+    </div>
   </div>
 );
 
-export default Upload;
+export default enhance(NotationNew);
