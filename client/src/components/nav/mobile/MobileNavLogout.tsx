1fd71eb303f45baf8008807b55a1537426c3757f
Jared Johnson <jared@nuts.com>
2017-12-23 13:15:24 -0500

    Finish the MobileNavLogout.

--- client/src/components/nav/mobile/MobileNavLogout.tsx
+++ client/src/components/nav/mobile/MobileNavLogout.tsx
@@ -1,13 +1,63 @@
 import * as React from 'react';
-import { Icon } from 'antd';
+import { compose, withState, withHandlers } from 'recompose';
+import { Modal, Icon } from 'antd';
+import { withSession } from 'enhancers';
 
-const MobileNavLogout = () => (
+const enhance = compose(
+  withSession,
+  withState('visible', 'setVisible', false),
+  withState('confirmLoading', 'setConfirmLoading', false),
+  withHandlers({
+    handleClick: props => event => {
+      props.setVisible(true);
+    },
+    handleOk: props => async event => {
+      props.setConfirmLoading(true);
+
+      try {
+        await props.session.dispatch.logout();
+
+        props.setConfirmLoading(false);
+
+        window.notification.success({
+          message: 'Logout',
+          description: 'successful'
+        });
+      } catch (error) {
+        window.notification.error({
+          message: 'Logout',
+          description: 'something went wrong'
+        });
+
+        console.error(error);
+
+        props.setConfirmLoading(false);
+      }
+    },
+    handleCancel: props => event => {
+      props.setVisible(false);
+    }
+  })
+);
+
+const MobileNavLogout = ({ confirmLoading, visible, handleClick, handleOk, handleCancel }) => (
   <div className="Nav--mobile__logout">
-    <Icon
-      className="Nav--mobile__icon"
-      type="logout"
-    />
+    <div onClick={handleClick}>
+      <Icon
+        className="Nav--mobile__icon"
+        type="logout"
+      />
+    </div>
+    <Modal
+      title="Logout"
+      visible={visible}
+      confirmLoading={confirmLoading}
+      onOk={handleOk}
+      onCancel={handleCancel}
+    >
+      <p>Are you sure you want to logout?</p>
+    </Modal>
   </div>
 );
 
-export default MobileNavLogout;
+export default enhance(MobileNavLogout);
