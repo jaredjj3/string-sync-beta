ae76f617115d4571490cc83d56045d2364a59604
Jared Johnson <jared@nuts.com>
2017-12-29 15:40:35 -0500

    Add and remove marker on mount and unmount.

--- client/src/components/fretboard/FretMarker.tsx
+++ client/src/components/fretboard/FretMarker.tsx
@@ -1,4 +1,18 @@
 import * as React from 'react';
+import { compose, lifecycle } from 'recompose';
+import { withFretboard } from 'enhancers';
+
+const enhance = compose(
+  withFretboard,
+  lifecycle({
+    componentDidMount(): void {
+      this.props.fretboard.state.instance.addMarker(this);
+    },
+    componentWillUnmount(): void {
+      this.props.fretboard.state.instance.removeMarker(this);
+    }
+  })
+);
 
 const FretMarker = ({ string, fret }) => (
   <div className="FretMarker">
