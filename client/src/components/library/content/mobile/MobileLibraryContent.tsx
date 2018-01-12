bfaa9c64b417d8b8bcffd526c4940a64f87020e5
Jared Johnson <jared@nuts.com>
2018-01-11 19:27:19 -0500

    Style the mobile library.

--- client/src/components/library/content/mobile/MobileLibraryContent.tsx
+++ client/src/components/library/content/mobile/MobileLibraryContent.tsx
@@ -7,12 +7,13 @@ import LibraryGrid from './LibraryGrid';
 
 const ContentHeader = () => (
   <div className="Library--mobile--contentHeader">
-    <div className="Library--mobile__headerSpacer" />
     <div className="Library--mobile__header">
       <div className="Library--mobile__header__logo">
         <LogoText style={{ fontSize: '24px' }}/>
       </div>
-      <LogoImage style={{ width: '48px', paddingTop: '10px' }}/>
+      <div className="Library--mobile__header__logo">
+        <LogoImage style={{ width: '48px' }}/>
+      </div>
     </div>
   </div>
 );
