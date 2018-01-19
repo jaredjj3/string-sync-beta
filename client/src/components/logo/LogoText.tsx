2bedea3b659f6df1abe074d4b1209c25812413e0
Jared Johnson <jared@nuts.com>
2018-01-18 21:45:39 -0500

    Use styled components in the LogoText components.

--- client/src/components/logo/LogoText.tsx
+++ client/src/components/logo/LogoText.tsx
@@ -1,9 +1,16 @@
 import * as React from 'react';
+import styled from 'styled-components';
+
+const LogoTextWrapper = styled.span`
+  font-size: 24px;
+  font-weight: 100;
+  letter-spacing: 3px;
+`;
 
 const LogoText = (props) => (
-  <span className="Logo--text" {...props}>
+  <LogoTextWrapper className="Logo--text" {...props}>
     STRING SYNC
-  </span>
+  </LogoTextWrapper>
 );
 
 export default LogoText;
--- client/src/components/logo/index.ts
+++ client/src/components/logo/index.ts
@@ -1,4 +1,2 @@
-import './_logo.less';
-
 export { default as LogoImage } from './LogoImage';
 export { default as LogoText } from './LogoText';
