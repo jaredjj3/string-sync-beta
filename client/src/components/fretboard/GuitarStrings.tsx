a3e0da3bf42faca0a42f575f60859b1fbf71a184
Jared Johnson <jared@nuts.com>
2018-01-20 14:20:36 -0500

    Use styled components in the GuitarStrings component.

--- client/src/components/fretboard/GuitarStrings.tsx
+++ client/src/components/fretboard/GuitarStrings.tsx
@@ -1,17 +1,33 @@
 import * as React from 'react';
 import { GuitarString } from './'
+import styled from 'styled-components';
+
+const GuitarStringsWrapper = styled.div`
+  .GuitarStrings {
+    position: absolute;
+    top: 0;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+    align-items: center;
+  }
+`;
+
+const GuitarStringsWrapped = styled.div``;
 
 const GuitarStrings = () => (
-  <div className="GuitarStrings">
-    {
-      Array(6).fill(null).map((_, string) => (
-        <GuitarString
-          key={`string-${string}`}
-          string={string}
-        />
-      ))
-    }
-  </div>
+  <GuitarStringsWrapper>
+    <GuitarStringsWrapped className="GuitarStrings">
+      {
+        Array(6).fill(null).map((_, string) => (
+          <GuitarString
+            key={`string-${string}`}
+            string={string}
+          />
+        ))
+      }
+    </GuitarStringsWrapped>
+  </GuitarStringsWrapper>
 );
 
 export default GuitarStrings;
