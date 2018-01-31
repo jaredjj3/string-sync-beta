49b131fab0dcbf2f9be043c5b10716391f936412
Jared Johnson <jared@nuts.com>
2018-01-31 07:48:53 -0500

    Force the user to acknowledge the beta notifier.

--- client/src/app/BetaNotifier.tsx
+++ client/src/app/BetaNotifier.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { compose, lifecycle } from 'recompose';
+import { compose, withHandlers, lifecycle } from 'recompose';
 import { Link } from 'react-router-dom';
 import { Button } from 'antd';
 import { hash } from 'ssUtil';
@@ -7,14 +7,20 @@ import { hash } from 'ssUtil';
 const UPDATED_AT = 'January 31';
 
 const enhance = compose(
+  withHandlers({
+    handleDismiss: props => event => {
+      localStorage.setItem('updatedAt', UPDATED_AT);
+    }
+  }),
   lifecycle({
     componentDidMount(): void {
       const updatedAt = localStorage.getItem('updatedAt');
 
       if (updatedAt !== UPDATED_AT) {
         window.notification.info({
-          duration: 10,
+          duration: null,
           message: 'StringSync BETA',
+          onClose: this.props.handleDismiss,
           description:
             <div>
               <div>updated at {UPDATED_AT}</div>
@@ -26,8 +32,6 @@ const enhance = compose(
               </div>
             </div>
         });
-
-        localStorage.setItem('updatedAt', UPDATED_AT);
       }
     }
   })
