d234b8f1c80e0c5f6dcc5b216659757866674f7e
Jared Johnson <jared@nuts.com>
2018-02-04 15:54:35 -0500

    Remove the roadmap.

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,10 +1,4 @@
 class StaticPagesController < ApplicationController
-  @@roadmap = YAML.load_file(File.join(Rails.root, "lib", "roadmap.yml")).freeze
-
-  def self.roadmap
-    @@roadmap
-  end
-
   def root
   end
 end
