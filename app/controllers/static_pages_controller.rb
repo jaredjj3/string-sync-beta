582bf2ea118b762feb69d6d2679fc6184bd312da
Jared Johnson <jared@nuts.com>
2018-02-04 12:58:46 -0500

    Create the roadmap and load it in the static pages.

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,10 @@
 class StaticPagesController < ApplicationController
+  @@roadmap = YAML.load_file(File.join(Rails.root, "lib", "roadmap.yml")).freeze
+
+  def self.roadmap
+    @@roadmap
+  end
+
   def root
   end
 end
