36c83bc8bdb3b2c1db201da068e0271d72058a73
Jared Johnson <jared@nuts.com>
2018-02-06 08:02:09 -0500

    Refactor the DirectiveExtractor.

--- client/src/services/directive/DirectiveExtractor.ts
+++ client/src/services/directive/DirectiveExtractor.ts
@@ -1,5 +1,5 @@
 import { Directive } from './';
-import { startsWith } from 'lodash';
+import { startsWith, partition } from 'lodash';
 
 // This class is constructed with a Vexflow stave object and then gets extract() 
 // called upon it. This mutates the stave object by excluding directive strings
@@ -8,6 +8,24 @@ class DirectiveExtractor {
   stave: any = null;
   directives: Array<Directive> = [];
   didExtraction: boolean = false;
+
+  // The argument is a single modifier from a vexflow tabNote or staveNote that has a
+  // text attribute.
+  static isDirective(modifier: any): boolean {
+    return startsWith(modifier.text, 'JSON=');
+  }
+
+  // The first array in the return value corresponds to directive modifiers and the
+  // second array corresponds to non-directive modifiers.
+  static partitionModifiers(modifiers: Array<any>): [Array<any>, Array<any>] {
+    return partition(modifiers, DirectiveExtractor.isDirective);
+  }
+
+  static createDirective(modifier: any, refs: any): Directive {
+    // FIXME: Overly complicated logic to hack JSON since Vextab handles commas differently
+    const directiveString = modifier.text.split('=')[1].replace(/\;/g, ',');
+    return new Directive(directiveString, refs);
+  }
 
   constructor(stave: any) {
     this.stave = stave;
@@ -21,34 +39,35 @@ class DirectiveExtractor {
       return this.directives;
     }
 
-    this.stave.tab_notes.forEach(tabNote => {
-      const rawDirectives = [];  // strings to be used to create directives
-      const nonDirectives = []; // other modifiers that are handled by Vexflow
-
-      // Fill the *Directives arrays
-      tabNote.modifiers.forEach(mod => {
-        if (startsWith(mod.text, 'JSON=')) {
-          // FIXME: Semicolons are used since Vextab's Artist class splits messages by commas.
-          // Look into another workaround that allows the user to use genuine json strings.
-          const json = mod.text.split('=')[1].replace(/\;/g, ',');
-          rawDirectives.push(json);
-        } else {
-          nonDirectives.push(mod);
-        }
-      });
-
-      // Effectively removes all of the rawDirectives from a given note's modifiers
-      tabNote.modifiers = nonDirectives;
-
-      // Take each of the rawDirective strings, create a Directive object from them,
-      // and push it onto the directives member variable.
-      rawDirectives.forEach(rawDirective => {
-        this.directives.push(new Directive(rawDirective));
-      });
-    });
+    for (let ndx of this.stave.tab_notes) {
+      const tabNote = this.stave.tab_notes[ndx];
+      const staveNote = this.stave.note_notes[ndx];
+      this._doExtraction(tabNote, staveNote);
+    }
 
     this.didExtraction = true;
     return this.directives;
+  }
+
+  private _doExtraction(tabNote, staveNote): void {
+    const [directiveMods, nonDirectiveMods] = DirectiveExtractor.
+      partitionModifiers(tabNote.modifiers);
+
+    // MUTATION!
+    // Effectively removes all of the rawDirectives from a given tabNote's modifiers
+    // The staveNote should not have any directive modifiers on it
+    tabNote.modifiers = nonDirectiveMods;
+
+    const refs = {
+      tabNote,
+      staveNote
+    };
+
+    // Create a directive from the directiveMods and push it onto the directives member
+    directiveMods.forEach(mod => {
+      const directive = DirectiveExtractor.createDirective(mod, refs);
+      this.directives.push(directive);
+    });
   }
 }
 
