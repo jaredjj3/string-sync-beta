8ff2c7e6f3387e2ec37701e26c28454d7d1392da
Jared Johnson <jared@nuts.com>
2018-02-09 22:44:57 -0500

    Debounce vextab updates in the vextabEditorComponent.

--- client/src/components/notation/edit/editor/VextabStringEditor.tsx
+++ client/src/components/notation/edit/editor/VextabStringEditor.tsx
@@ -1,26 +1,59 @@
 import * as React from 'react';
-import { compose, withHandlers, withProps } from 'recompose';
+import { withRouter } from 'react-router-dom';
+import { compose, withHandlers, withProps, withState, withPropsOnChange, lifecycle } from 'recompose';
 import { Input, Alert } from 'antd';
 import { withTab, withNotation } from 'enhancers';
+import { debounce } from 'lodash';
 import styled from 'styled-components';
 
 const { TextArea } = Input;
 
+let setNotationVextabString = (props: any) => {
+  const vextabString = props.editorVextabString;
+  const notation = Object.assign({}, props.notation.state);
+  notation.vextabString = vextabString;
+  props.notation.dispatch.setNotation(notation);
+};
+
+setNotationVextabString = debounce(setNotationVextabString, 250);
+
 const enhance = compose(
   withTab,
   withNotation,
+  withRouter,
+  withState('editorVextabString', 'setEditorVextabString', null),
+  withState('isInitialized', 'setIsInitialized', false),
   withProps(props => ({
     getParseError: () => {
       const tab = props.tab.state.instance;
       return tab ? tab.error : null;
     }
   })),
+  withProps(props => ({
+    setNotationVextabString: () => {
+      setNotationVextabString(props);
+    }
+  })),
   withHandlers({
     handleChange: props => event => {
-      const vextabString = event.target.value;
-      const notation = Object.assign({}, props.notation.state);
-      notation.vextabString = vextabString;
-      props.notation.dispatch.setNotation(notation);
+      props.setEditorVextabString(event.target.value);
+    }
+  }),
+  lifecycle({
+    componentWillReceiveProps(nextProps: any): void {
+      const idFromParams = parseInt(nextProps.match.params.id, 10);
+      const isNotationFetched = idFromParams === nextProps.notation.state.id
+      const notationVextabString = nextProps.notation.state.vextabString;
+      const editorVextabString = nextProps.editorVextabString;
+
+      if (isNotationFetched) {
+        if (!nextProps.isInitialized) {
+          nextProps.setEditorVextabString(nextProps.notation.state.vextabString);
+          nextProps.setIsInitialized(true);
+        } else if (notationVextabString !== editorVextabString) {
+          nextProps.setNotationVextabString();
+        }
+      }
     }
   })
 );
@@ -53,12 +86,12 @@ const ParseError = ({ parseError }) => {
   }
 }
 
-const VextabStringEditor = ({ getParseError, notation, handleChange }) => (
+const VextabStringEditor = ({ getParseError, editorVextabString, handleChange }) => (
   <VextabStringEditorOuter>
     <ParseError parseError={getParseError()} />
     <TextArea
       placeholder="Write Vextab here..."
-      value={notation.state.vextabString}
+      value={editorVextabString}
       autosize={{ minRows: 5, maxRows: 10 }}
       onChange={handleChange}
     />
