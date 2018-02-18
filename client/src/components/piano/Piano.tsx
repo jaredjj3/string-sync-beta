026d7801b8e978b5c1dd27bdeb15bd023b427e2c
Jared Johnson <jared@nuts.com>
2018-02-18 14:50:26 -0500

    Use identity and hasGlobalProps enhancers in the Piano component.

--- client/src/components/piano/Piano.tsx
+++ client/src/components/piano/Piano.tsx
@@ -1,21 +1,15 @@
 import * as React from 'react';
 import { compose, withState, lifecycle, branch, renderNothing } from 'recompose';
 import { PianoKeys, PianoController } from './';
+import { hasGlobalProps, identity } from 'enhancers';
 import styled from 'styled-components';
 
 const enhance = compose(
   withState('isVisible', 'setVisibility', false),
-  lifecycle({
-    componentDidMount(): void {
-      window.ss.maestro.pianoProps = this.props;
-    },
-    componentWillUnmount(): void {
-      window.ss.maestro.pianoProps = null;
-    }
-  }),
+  hasGlobalProps('piano', () => window.ss.globalProps),
   branch(
     props => props.isVisible,
-    i => i,
+    identity,
     renderNothing
   )
 );
